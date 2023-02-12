7c65ed69ee16ba6d8b2fca1c2915f84a7ddaa2e0
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-02-12 16:25:16 +0100

    fix port

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ def generate_image():
         return jsonify({"error": "Failed to generate image"}), 500
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
