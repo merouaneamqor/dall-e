7fc368b868e2ac0e20cf379442b91a2e5fc99620
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-02-12 17:25:17 +0100

    Okay json

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def generate_image():
     else:
         # Try to convert the request data to JSON
         try:
-            data = jsonify(request)
+            data = json.loads(request.data)
         except:
             # Return an error message if the conversion fails
             return jsonify({"error": "Content-Type must be set to application/json"}), 400
